2920526778238076e7c2a8aaffcc2816fbc0a9e8
stimsina <timsinasudhan9@gmail.com>
2019-01-06 19:04:16 +1100

    get all participant and add participant hooked up with backend

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,37 @@
 <template>
-  <div class="home">
+  <div class="home container">
     <i class="material-icons green darken-2  newParticipant" @click.prevent="goToAddParticipants">add</i>
     <div class="home participant container " v-for="(participant,index) in participantsList" :key='index'>
       <div class="card-panel">
-        <span>{{participant.name}}</span>
-        <span>{{participant.voteID}}</span>
+        <h6>{{participant.firstName}}  {{participant.lastName}}</h6>
+        {{participant.age}}
+        <textarea v-model="participant.description" disabled></textarea>
+        <button class="btn">Vote</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from '@/axios/init'
 export default {
   name: 'Home',
   data () {
     return {
-      participantsList:[{name:'Sudhan',voteID:20},{name:'John',voteID:30}],
+      participantsList:null,
     }
   },
   methods:{
     goToAddParticipants(){
       this.$router.push({name:'AddParticipant'})
     }
+  },
+  created(){
+    axios.get('/participants').then(resposne=>{
+      this.participantsList=resposne.data
+    }).catch(err=>{
+      console.log(err)
+    })
   }
 }
 </script>
